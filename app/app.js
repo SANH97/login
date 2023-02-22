3c3c737c667ca18cbd6d62ff2fcde5c8fd8cfb97
tkdgml97 <97tkdgml@naver.com>
2023-02-22 15:45:20 +0900

    프런트 단 java

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ const app =express();
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
+app.use(express.static(`${__dirname}/src/public`))
+
 const home = require("./src/routes/home");
 app.use("/",home); //use-> 미들 웨어
 
